6fc0688f62366cd0073b83f69a048c08abc2643f
MohammadAli <mzov939@gmail.com>
2021-08-30 00:59:53 +0430

    Delete build directory

--- build/config/session.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var grammy_1 = require("grammy");
-var session = new grammy_1.MemorySessionStorage();
-exports.default = session;
