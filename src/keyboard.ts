28b0cd83c95b9300a47f80bd8ccdadd0e7aaec89
mmdzov <mzov939@gmail.com>
2021-08-31 07:02:04 +0430

    Refactoring

--- src/keyboard.ts
+++ src/keyboard.ts
@@ -2,9 +2,8 @@ import { Keyboard } from "grammy";
 
 class keyboard {
   userKeyboard() {
-    let main = ["قفل به کانال🔐", "مانیتورینگ📈", "ارسال🕊"];
+    let main = ["قفل به کانال🔐", "مانیتورینگ📈", "افزودن"];
     let channel = ["کانال جدید", "مشاهده کانال ها", "حذف کانال", "بازگشت"];
-    let send = ["ارسال به کانال", "ارسال به همینجا"];
     let upload = ["بازگشت", "آپلود محتوا"];
     let cancel = "بازگشت";
     let mainKeyboard = new Keyboard().add(...main.reverse());
@@ -15,14 +14,12 @@ class keyboard {
       .text(channel[1])
       .text(channel[0])
       .row();
-    let sendKeyboard = new Keyboard().add(...send);
     let cancelKeyboard = new Keyboard().text(cancel);
     let uploadKeyboard = new Keyboard().add(...upload);
     return {
       mainKeyboard,
       channelKeyboard,
       cancelKeyboard,
-      sendKeyboard,
       uploadKeyboard,
     };
   }
